40b829bc8efdabdf339dc0ed2b879dd82cbe38d6
李迪 <lidi@xiaojiang.cc>
2020-12-20 23:15:25 +0800

    feature: RGObserver

---
+++ RGObserver/RGObserver.h
@@ -0,0 +1,13 @@
+//
+//  RGObserver.h
+//  Pods
+//
+//  Created by renge on 2020/12/18.
+//
+
+#ifndef RGObserver_h
+#define RGObserver_h
+
+#import <RGObserver/NSObject+RGObserver.h>
+
+#endif /* RGObserver_h */
